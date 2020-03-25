586c8a6cf087d26ebd955688e674b59f60230664
Ken Dreyer <kdreyer@redhat.com>
2020-03-25 13:03:50 -0600

    tests: test variant's scrape_error_explanation() method
    Remove the trivial "variant" assertion, and replace it with our first
    real unit test for this module.
    
    Ensure that scrape_error_explanation() can screen-scrape the proper
    error string from an HTML snippet.

--- tests/test_errata_tool_variant.py
+++ tests/test_errata_tool_variant.py
@@ -1,5 +1,18 @@
-import errata_tool_variant
+from errata_tool_variant import scrape_error_explanation
 
 
-def test_simple():
-    assert errata_tool_variant
+class FakeErrorResponse(object):
+    pass
+
+
+def test_scrape_error_explanation():
+    response = FakeErrorResponse()
+    response.text = '''
+    <div id="errorExplanation" class="errorExplanation">
+    <h2>1 error prohibited this variant from being saved</h2>
+    <p>There were problems with the following fields:</p>
+    <ul><li>Variant push targets is invalid</li></ul></div>
+'''
+    result = scrape_error_explanation(response)
+    expected = ['Variant push targets is invalid']
+    assert result == expected
